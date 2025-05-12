bfc02339d3aa409f522d61ae8c83e6bf9a2df906
Makmanu <vfrfv0640@gmail.com>
2025-05-12 17:54:50 +0300

    code optimization

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def main():
     dt = 0
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)
+        updatable.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
-        player1.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(144) / 1000
 
